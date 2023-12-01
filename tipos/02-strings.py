c7b1170284c710e2d3ed56ca82c38e8c8951d849
Airam A. <airam.a.g.85@gmail.com>
2023-12-01 22:49:01 +0000

    Strings

---
+++ tipos/02-strings.py
@@ -0,0 +1,17 @@
+"""
+Ejercicio con Strings
+"""
+
+NOMBRE_CURSO = "Ultimate Python"
+DESCRIPCION_CURSO = """
+Ultimate Python,
+este curso contempla todos los detalles
+que necesitas aprender para encontrar
+un trabajo como programador.
+"""
+print(len(NOMBRE_CURSO))
+print(NOMBRE_CURSO[0])
+print(NOMBRE_CURSO[0:8])
+print(NOMBRE_CURSO[9:])
+print(NOMBRE_CURSO[:8])
+print(NOMBRE_CURSO[:])
