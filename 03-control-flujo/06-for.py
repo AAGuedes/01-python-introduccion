0d994fe6e66a479f2a1e391ff2ca28bd6bd82b90
Airam A. <airam.a.g.85@gmail.com>
2023-12-02 20:51:18 +0000

    Iterables

--- 03-control-flujo/06-for.py
+++ 03-control-flujo/06-for.py
@@ -1,13 +1,16 @@
 """
-Operador For
+Operador For, For else e Iterables
 """
 
 BUSCAR = 10
 
-for numero in range(5):
-    print(numero)
-    if numero == BUSCAR:
-        print("Encontrado", BUSCAR)
-        break
-else:
-    print("No encontrado")
+# for numero in range(5):
+#     print(numero)
+#     if numero == BUSCAR:
+#         print("Encontrado", BUSCAR)
+#         break
+# else:
+#     print("No encontrado")
+
+for char in "Ultimate Python":
+    print(char)
