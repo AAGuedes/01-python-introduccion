3930649ce7e7843152dd2776ab958c13d407e85b
Airam A. <airam.a.g.85@gmail.com>
2023-12-02 20:57:04 +0000

    While

---
+++ 03-control-flujo/07-while.py
@@ -0,0 +1,13 @@
+"""
+Operador While
+"""
+
+# NUMERO = 1
+# while NUMERO < 10:
+#     print(NUMERO)
+#     NUMERO += 1
+
+comando = ""
+while comando.lower() != "salir":
+    comando = input("$ ")
+    print(comando)
