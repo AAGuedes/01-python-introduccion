3dfdbd700571d6d2c1b184ec91655dbcfc0b648c
Airam A. <airam.a.g.85@gmail.com>
2023-12-02 17:08:24 +0000

    If, else y elif

---
+++ 03-control-flujo/02-if.py
@@ -0,0 +1,16 @@
+"""
+Comparador IF
+"""
+
+EDAD = 70
+
+if EDAD > 64:
+    print("Puedes ver la película con un grand descuento")
+elif EDAD > 54:
+    print("Puedes ver la película con descuento")
+elif EDAD > 17:
+    print("Puede ver la pelicula")
+else:
+    print("No puedes entrar")
+
+print("Comprobación terminada")
