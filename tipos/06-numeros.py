abc8b6fd78c3ef8969f5267637c587dea41a93be
Airam A. <airam.a.g.85@gmail.com>
2023-12-02 11:58:10 +0000

    Números

---
+++ tipos/06-numeros.py
@@ -0,0 +1,21 @@
+"""
+Numeros en Python
+"""
+
+ENTERO = 2          # Integer
+DECIMAL = 1.5       # Float
+IMAGINARIO = 2 + 2j # j = raíz cuadrada de -1
+
+ENTERO = ENTERO + 2
+print("ENTERO", ENTERO)
+
+ENTERO += 2
+print("ENTERO", ENTERO)
+
+print(1 + 3)    # Suma
+print(1 - 3)    # Resta
+print(1 * 3)    # Multiplicación
+print(1 / 3)    # Divisíon con decimales
+print(1 // 3)   # División sin decimales
+print(8 % 3)    # Obtiene el resto de la division
+print(2 ** 3)   # Potencia
