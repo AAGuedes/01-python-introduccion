08750e0e1ee65868cd5606e59ccc786c0d66e163
Airam A. <airam.a.g.85@gmail.com>
2023-12-02 20:25:55 +0000

    Operador ternario

---
+++ 03-control-flujo/03-if-ternario.py
@@ -0,0 +1,14 @@
+"""
+Operador ternario
+"""
+
+EDAD = 22
+
+MENSAJE = "Mayor de edad" if EDAD > 17 else "Menor de edad"
+
+# if EDAD > 17:
+#     MENSAJE = "Mayor de edad"
+# else:
+#     MENSAJE = "Menor de edad"
+
+print(MENSAJE)
