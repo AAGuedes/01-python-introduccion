492c38202cbb73e6dc8466a0d6ddf439f9e5fd0d
Airam A. <airam.a.g.85@gmail.com>
2023-12-03 19:14:46 +0000

    Return

---
+++ 04-funciones/04-return.py
@@ -0,0 +1,15 @@
+"""
+Return
+"""
+
+
+def suma(a, b):
+    """
+    Funciona que suma dos números y retorna el resultado
+    """
+    resultado = a + b
+    return resultado
+
+
+c = suma(1, 2)
+print(c)
