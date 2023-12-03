502720e44d3458da3c64925deafa1c865da81411
Airam A. <airam.a.g.85@gmail.com>
2023-12-03 13:44:00 +0000

    Argumentos opcionales

--- 04-funciones/01-funciones.py
+++ 04-funciones/01-funciones.py
@@ -1,13 +1,15 @@
 """
 Introducción a las funciones
 Parámetros y argumentos
+Argumentos opcionales
 """
 
 
-def prueba(name, surname):
+def prueba(name, surname = ""):
     """Función de prueba que recibe un nombre, apellido y lo imprime"""
     print("Hello World!")
     print(f"Welcome {name} {surname}")
 
 
 prueba("John", "Doe")
+prueba("Jane")
