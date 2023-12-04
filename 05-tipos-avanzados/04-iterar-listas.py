564ce0178da763872357d42d609d2d7d049638d9
Airam A. <airam.a.g.85@gmail.com>
2023-12-04 00:01:30 +0000

    Iterar listas

---
+++ 05-tipos-avanzados/04-iterar-listas.py
@@ -0,0 +1,8 @@
+"""
+Iterar listas
+"""
+
+mascotas = ["Pelusa", "Pulga", "Toby", "Kity"]
+
+for indice, mascota in enumerate(mascotas):  # Enumerate retorna una Tupla y se puede desempaquetar
+    print(indice, mascota)
