8acd2d73e9ada2c6f67e4678d8c57935d2933b25
Airam A. <airam.a.g.85@gmail.com>
2023-12-04 16:46:47 +0000

    Filas

---
+++ 05-tipos-avanzados/13-filas.py
@@ -0,0 +1,19 @@
+"""
+Filas
+"""
+from collections import deque
+
+fila = deque([1, 2, 3, 4])
+fila.append(5)
+print(fila)
+
+# Elimina el primer elemento por la izquierda
+fila.popleft()
+fila.popleft()
+fila.popleft()
+fila.popleft()
+fila.popleft()
+print(fila)
+
+if not fila:
+    print("Fila vacía")
