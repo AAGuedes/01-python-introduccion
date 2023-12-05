4565198955e36fd375a090a8bc68dfae5e7f72f5
Airam A. <airam.a.g.85@gmail.com>
2023-12-05 23:49:07 +0000

    Paquetes con nombres dinámicos

--- 08-modulos/app.py
+++ 08-modulos/app.py
@@ -1,16 +1,17 @@
 "Clase app"
 
 # from usuarios.acciones import guardar, pagar_impuestos
-# from usuarios.impuestos.utilidades import pagar_impuestos
-import usuarios
+from usuarios.impuestos.utilidades import pagar_impuestos
+# import usuarios
 
-# pagar_impuestos()
+pagar_impuestos()
+print("app.py", __name__)
 
-print(dir(usuarios))
-print(usuarios.__name__)
-print(usuarios.__package__)
-print(usuarios.__path__)
-print(usuarios.__file__)
+# print(dir(usuarios))
+# print(usuarios.__name__)
+# print(usuarios.__package__)
+# print(usuarios.__path__)
+# print(usuarios.__file__)
 
 # Otra forma de importar es la siguiente:
 # import usuarios.acciones
--- 08-modulos/usuarios/impuestos/utilidades.py
+++ 08-modulos/usuarios/impuestos/utilidades.py
@@ -1,8 +1,14 @@
 "Clase utilidades"
-from ..gestion.crud import guardar
+# from ..gestion.crud import guardar
+
+print("utilidades.py", __name__)
 
 
 def pagar_impuestos():
     "Función pagar impuestos"
     print("Pagando impuestos")
-    guardar()
+    # guardar()
+
+
+if __name__ == "__main__":
+    print("Tarea de mantenimiento")
