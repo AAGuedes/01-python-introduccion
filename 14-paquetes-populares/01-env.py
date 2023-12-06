f163c029bf34612b64e0d4bf9eaef8adc28b4b5c
Airam A. <airam.a.g.85@gmail.com>
2023-12-06 22:57:00 +0000

    Sendgrid

--- 14-paquetes-populares/01-env.py
+++ 14-paquetes-populares/01-env.py
@@ -1,5 +1,26 @@
 # Variables de entorno
 import os
+from sendgrid.helpers.mail import Mail
+from sendgrid import SendGridAPIClient
 
-apikey = os.environ.get("SENGRID_API_KEY")
-print(apikey)
+
+email = os.environ.get("SENDGRID_EMAIL")
+
+mensaje = Mail(
+    from_email=email,
+    to_emails=email,
+    subject="Correo de prueba",
+    html_content="Curso de <b>Ultimate Python</b>"
+)
+
+try:
+    apikey = os.environ.get("SENGRID_API_KEY")
+    sg = SendGridAPIClient(apikey)
+    respuesta = sg.send(mensaje)
+    print(
+        respuesta.status_code,
+        respuesta.headers,
+        respuesta.body
+    )
+except Exception as e:
+    print(e)
