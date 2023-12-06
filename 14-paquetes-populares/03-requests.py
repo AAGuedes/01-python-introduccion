08e22b19607a78e31650dd2dcd5ea22dbe504496
Airam A. <airam.a.g.85@gmail.com>
2023-12-06 23:27:25 +0000

    Requests

--- 14-paquetes-populares/03-requests.py
+++ 14-paquetes-populares/03-requests.py
@@ -1,1 +1,34 @@
-"https://api.holamundo.io/users/"
+import requests
+
+# GET
+url = "https://jsonplaceholder.typicode.com/users"
+r = requests.get(url, timeout=10)
+r = r.json()
+for user in r:
+    print(user["name"])
+
+
+# GET
+url = "https://jsonplaceholder.typicode.com/users/1"
+r = requests.get(url, timeout=10)
+print(r.json())
+
+
+# POST
+url = "https://jsonplaceholder.typicode.com/users"
+user = {
+    "id": 11,
+    "name": "John Doe"
+}
+r = requests.post(url, timeout=10, data=user)
+print(r.status_code)
+
+
+# PUT y PATCH
+url = "https://jsonplaceholder.typicode.com/users/1"
+user = {
+    "id": 1,
+    "name": "John Doe"
+}
+r = requests.put(url, timeout=10, data=user)
+print(r.status_code)
