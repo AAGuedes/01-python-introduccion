1f6349f21dc7bdf18040a0405b4087dd9d6e31b2
Airam A. <airam.a.g.85@gmail.com>
2023-12-07 17:09:53 +0000

    Error 404

--- 15-django/productos/views.py
+++ 15-django/productos/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 
@@ -30,8 +30,20 @@ def index(request):
 
 
 def detalle(request, producto_id):
-    producto = Producto.objects.get(id=producto_id)
-    
+    # En caso de que se busque un elemento que no existe lanza 404
+    # try:
+    #     producto = Producto.objects.get(id=producto_id)
+
+    #     return render(
+    #         request,
+    #         'detalle.html',
+    #         context={'producto': producto}
+    #     )
+    # except Producto.DoesNotExist:
+    #     raise Http404()
+
+    producto = get_object_or_404(Producto, id=producto_id)
+
     return render(
         request,
         'detalle.html',
