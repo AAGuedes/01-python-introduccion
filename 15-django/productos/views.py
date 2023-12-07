9b8f6c55b944fa71e1960d3ae63599f187a17d9b
Airam A. <airam.a.g.85@gmail.com>
2023-12-07 16:22:33 +0000

    Plantillas

--- 15-django/productos/views.py
+++ 15-django/productos/views.py
@@ -1,19 +1,29 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Producto
 
 
+# def index(request):
+# Retorna todos los elementos
+# productos = Producto.objects.all()
+# productos = Producto.objects.all().values()
+
+# Filtra por puntaje igual a 3
+# productos = Producto.objects.filter(puntaje=3)
+
+# Filtra por puntaje mayor o igual a 3
+# productos = Producto.objects.filter(puntaje__gte=3)
+
+# Filtra por id igual a 1
+# productos = Producto.objects.get(id=1)
+# print(productos)
+# return JsonResponse(list(productos), safe=False)
+
 def index(request):
-    # Retorna todos los elementos
-    productos = Producto.objects.all().values()
+    productos = Producto.objects.all()
 
-    # Filtra por puntaje igual a 3
-    # productos = Producto.objects.filter(puntaje=3)
-
-    # Filtra por puntaje mayor o igual a 3
-    # productos = Producto.objects.filter(puntaje__gte=3)
-
-    # Filtra por id igual a 1
-    # productos = Producto.objects.get(id=1)
-    # print(productos)
-    return JsonResponse(list(productos), safe=False)
+    return render(
+        request,
+        'index.html',
+        context={'productos': productos}
+    )
