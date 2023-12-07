cc684613dd0208c6932b3dc983b6cc34324d347f
Airam A. <airam.a.g.85@gmail.com>
2023-12-07 15:08:01 +0000

    Actualizando modelos

--- 15-django/productos/models.py
+++ 15-django/productos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Categoria(models.Model):
@@ -18,3 +19,4 @@ class Producto(models.Model):
         Categoria,
         on_delete=models.CASCADE
     )
+    creado_en: models.DateTimeField(default=timezone.now)
