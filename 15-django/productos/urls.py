9ba09d75b00baad96ce488eef8c2eef446d2f661
Airam A. <airam.a.g.85@gmail.com>
2023-12-07 17:24:29 +0000

    Links

--- 15-django/productos/urls.py
+++ 15-django/productos/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from . import views
 
+app_name = 'productos'
 
 urlpatterns = [
     path(
         '',
         views.index,
-        name='index'
+        name='productos'
     ),
     path(
         '<int:producto_id>',
         views.detalle,
-        name='producto_detalle'
+        name='detalle'
     )
 ]
