a76d6ae6bad57869207fb9e4f55a08f91137ba29
Airam A. <airam.a.g.85@gmail.com>
2023-12-07 15:44:08 +0000

    Iteractuando con la base de datos

--- 15-django/productos/views.py
+++ 15-django/productos/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Producto
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse('Hola Mundo!')
+    # Retorna todos los elementos
+    productos = Producto.objects.all().values()
+
+    # Filtra por puntaje igual a 3
+    # productos = Producto.objects.filter(puntaje=3)
+
+    # Filtra por puntaje mayor o igual a 3
+    # productos = Producto.objects.filter(puntaje__gte=3)
+
+    # Filtra por id igual a 1
+    # productos = Producto.objects.get(id=1)
+    # print(productos)
+    return JsonResponse(list(productos), safe=False)
