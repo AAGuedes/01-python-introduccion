26557123137cd213fd75b4eeb0d99495688b1d22
Airam A. <airam.a.g.85@gmail.com>
2023-12-07 01:48:29 +0000

    Models

--- 15-django/productos/models.py
+++ 15-django/productos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    # CASCADE: Elimina el producto
+    # PROTECT: Lanza un error
+    # RESTRICT: Solo elimina si no existen productos
+    # SET_NULL: Actualiza a valor nulo
+    # SET_DEFAULT: Asigna valor por defecto
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE
+    )
